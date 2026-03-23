5ebccfeb13f0f6083b46b968f0f0abb5257b65dc
Melina <melina.dmec@gmail.com>
2026-03-23 18:04:24 +0100

    cambios alumno B

--- assets/js/alumnoB.js
+++ assets/js/alumnoB.js
@@ -1,16 +1,10 @@
 // Alumno B
 
-// Arreglo de poderes
 const poderes = ["Vuelo", "Fuerza", "Telepatía"];
 
-// Elementos del DOM
 const poder = document.getElementById("hero-poder");
-const boton = document.getElementById("btn-cargar");
+const botonB = document.getElementById("btn-cargar");
 
-// Evento
-boton.addEventListener("click", function () {
-    // Último elemento del arreglo
-    const ultimoPoder = poderes[poderes.length - 1];
-
-    poder.innerText = ultimoPoder;
+botonB.addEventListener("click", function () {
+    poder.innerText = poderes[2];
 });
